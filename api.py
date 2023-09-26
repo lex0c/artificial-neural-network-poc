0bb85a433db90cddf19371124f78cd2681e7e449
leo <lex0c@proton.me>
2023-09-26 15:27:02 -0300

    implementing train method

--- api.py
+++ api.py
@@ -4,14 +4,29 @@ from datetime import datetime
 import json
 import logging
 
+from feedforward import FeedForward
+from etc import normalize_minmax, save_model, load_model
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+nn_agent = FeedForward(verbose=False)
+nn_agent.add_layer(num_inputs=4, num_neurons=4, act_fn="relu")
+nn_agent.add_layer(num_inputs=4, num_neurons=4, act_fn="relu")
+nn_agent.add_layer(num_inputs=4, num_neurons=1, act_fn="linear")
+
+
 async def echo(websocket, path):
     try:
         logger.info("%s - Client Connected: %s", datetime.now(), websocket.remote_address)
+
+        state = await websocket.recv() # Receive the initial state from the client.
+        state = json.loads(state)
+
+        action = nn_agent.select_action(normalize_minmax(state)) # The model selects an action based on the current state.
+        await websocket.send(json.dumps(action))  # Send the action to the client.
 
         async for message in websocket:
             logger.info("%s - Received: %s", datetime.now(), message)
@@ -29,9 +44,17 @@ async def echo(websocket, path):
                 logger.error("%s - %s", datetime.now(), ve)
                 continue
 
-            await websocket.send(json.dumps(data))
+            next_state = normalize_minmax(data['next_state'])
 
-            logger.info("%s - Sent: %s", datetime.now(), data)
+            nn_agent.learn(state, action, data['reward'], next_state, data['done'])  # The model learns from the experience.
+            save_model('api_agent', nn_agent.layers)
+
+            if not data['done']:
+                action = nn_agent.select_action(next_state) # The model selects an action based on the current state.
+                await websocket.send(json.dumps(action))  # Send the action to the client.
+                logger.info("%s - Sent: %s", datetime.now(), action)
+
+            state = next_state
     except websockets.ConnectionClosed as e:
         logger.warning(e)
     except Exception as e:
--- feedforward.py
+++ feedforward.py
@@ -1,5 +1,6 @@
 import math
 import random
+#import numpy as np
 
 from etc import relu, sigmoid, softmax, linear
 
@@ -76,5 +77,45 @@ class FeedForward:
             values = layer(values, layer_param["weights"], layer_param["biases"], layer_param["act_fn"], self.verbose)
 
         return values
+
+    def select_action(self, state, epsilon=0.1, threshold=0.5):
+        if random.uniform(0, 1) < epsilon:
+            return random.choice([0, 1])
+
+        layer_output = self.forward(state)
+
+        if layer_output:
+            return 1 if layer_output[0] >= threshold else 0
+
+        return random.choice([0, 1])
+
+    def learn(self, state, action, reward, next_state, done, learning_rate=0.1, discount_factor=0.99):
+        # Predict Q-values for current state
+        q_values = self.forward(state)
+
+        # Copy Q-values as Target Q-values
+        target_q_values = list(q_values)
+
+        if done:
+            target_q_values[0] = reward # Assume the reward is correctly calculated externally
+        else:
+            # Predict Q-values for the next state
+            next_q_values = self.forward(next_state)
+
+            # Update the target Q-value for the action taken
+            target_q_values[0] = reward + discount_factor * max(next_q_values)
+
+        # Compute the error
+        error = [t - q for t, q in zip(target_q_values, q_values)]
+
+        # Update weights and biases
+        for i in reversed(range(len(self.layers))):
+            layer = self.layers[i]
+
+            for neuron_idx, weights in enumerate(layer["weights"]):
+                for weight_idx, weight in enumerate(weights):
+                    weights[weight_idx] += learning_rate * error[0] * state[weight_idx]
+
+                layer["biases"][neuron_idx] += learning_rate * error[0]
 
 
